0c625f3f6cb2b2ac16b7bf161fa5a60d10fe160d
inferno-8 <rahuldevopseng@gmail.com>
2025-08-10 00:20:36 +0530

    Added rg

--- main.tf
+++ main.tf
@@ -22,7 +22,6 @@ provider "azurerm" {
   subscription_id = "4a8937a7-b294-4a1f-8d31-79bbb1a4c17f"
 }
 
-
 resource "azurerm_resource_group" "rj" {
   name     = "RRJ-ResourceGroup"
   location = "East US"
