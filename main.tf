8d14cd06b36ae2283778c6b34b3f25361e0f09c2
inferno-8 <rahuldevopseng@gmail.com>
2025-08-10 00:05:52 +0530

    added new pipeline

---
+++ main.tf
@@ -0,0 +1,29 @@
+terraform {
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "4.32.0"
+    }
+  }
+
+
+  backend "azurerm" {
+      resource_group_name   = "RRJ-ResourceGroup"        
+      storage_account_name  = "rrj8stg"
+      container_name        = "rrjcontainer"
+      key                   = "devops.terraform.tfstate"
+    }
+
+}
+
+
+provider "azurerm" {
+  features {}
+  subscription_id = "4a8937a7-b294-4a1f-8d31-79bbb1a4c17f"
+}
+
+
+resource "azurerm_resource_group" "rj" {
+  name     = "RRJ-ResourceGroup"
+  location = "East US"
+}
